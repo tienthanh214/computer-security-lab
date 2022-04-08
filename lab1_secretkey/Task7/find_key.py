5359d9d432ccb6535279fc3cdb2704bf01a70fa8
tienthanh214 <tienthanhcute214@gmail.com>
2022-04-08 06:13:02 -0400

    re-upload task 7

--- lab1_secretkey/Task7/find_key.py
+++ lab1_secretkey/Task7/find_key.py
@@ -1,12 +1,10 @@
 # import library 
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
-
 # constant 
 PLAINTEXT = b"This is a top secret."
 CIPHERTEXT = bytes.fromhex("764aa26b55a4da654df6b19e4bce00f4ed05e09346fb0e762583cb7da2ac93a2")
 iv = bytes.fromhex("aabbccddeeff00998877665544332211")
-
 # read dictionary
 with open('words.txt') as f:
 	word_list = f.read().split('\n')
@@ -16,9 +14,9 @@ def decrypt(text, key):
 	return cipher.encrypt(pad(text, 16))
 
 if __name__ == '__main__':
-	for key in word_list:
-		key_pad = key + '#' * (16 - len(key) % 16)
-		key_pad = bytearray(key_pad, 'utf-8')
+	for key in word_list: # iterate over dictionary
+		key_pad = key + '#' * (16 - len(key) % 16) 	# add '#' paddng
+		key_pad = bytearray(key_pad, 'utf-8')		# convert to byte array
 		if (decrypt(PLAINTEXT, key_pad) == CIPHERTEXT):
 			print('Key:', key)
 			
